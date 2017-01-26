44dc7aea48ecf015eeb74baab7d20511351d34c7
Åke Forslund <ake.forslund@gmail.com>
2017-01-26 07:50:20 +0100

    First commit.

---
+++ README.md
@@ -0,0 +1,24 @@
+Command Skill
+=====================
+
+A Simple mycroft skill for running shell scripts and other commands. The commands will run quietly without any confirmation from Mycroft.
+
+## Usage
+
+*Hey Mycroft, launch command echo TEST*
+
+*Hey Mycroft, run script generate report*
+
+## Configuration
+
+The skill can be configured to run scripts from easily pronouncable human utterances, such as "generate report" in the example.
+
+```json
+  "CmdSkill": {
+      "alias": {
+        "generate report": "/home/forslund/scripts/generate_report.sh",
+      }
+  }
+```
+
+The configuration above will launch `/home/forslund/scripts/generate_report.sh` when the second utterance under usage is invoked.
---
+++ __init__.py
@@ -0,0 +1,63 @@
+import sys
+from os.path import dirname, abspath, basename
+import subprocess
+
+from mycroft.skills.core import MycroftSkill
+from adapt.intent import IntentBuilder
+from mycroft.messagebus.message import Message
+
+
+import time
+from time import mktime
+
+from pwd import getpwnam
+import os
+from os.path import dirname
+from mycroft.util.log import getLogger
+
+sys.path.append(abspath(dirname(__file__)))
+
+logger = getLogger(abspath(__file__).split('/')[-2])
+__author__ = 'forslund'
+
+def set_user(uid, gid):
+    logger.info('Setting group and user to ' + str(gid) + ':' + str(uid))
+    os.setgid(gid)
+    os.setuid(uid)
+
+class CmdSkill(MycroftSkill):
+    def __init__(self):
+        super(CmdSkill, self).__init__('CmdSkill')
+        self.uid = None
+        self.gid = None
+        self.alias = {}
+        if self.config:
+            user = self.config.get('user')
+            if user:
+                pwnam = getpwnam(user)
+                self.uid = pwnam.pw_uid
+                self.gid = pwnam.pw_gid
+            self.alias = self.config.get('alias', {})
+
+    def initialize(self):
+        self.load_data_files(dirname(__file__))
+
+        intent = IntentBuilder('RunScriptCommandIntent')\
+                 .require('Script')\
+                 .build()
+        self.register_intent(intent, self.run)
+
+        self.emitter.on('CmdSkillRun', self.run)
+    
+    def run(self, message):
+        script = message.data.get('Script')
+        script = self.alias.get(script, script)
+        args = script.split(' ')
+        if self.uid and self.gid:
+            p = subprocess.Popen(args, preexec_fn=set_user(self.uid, self.gid))
+        else:
+            p = subprocess.Popen(args)
+
+
+def create_skill():
+    return CmdSkill()
